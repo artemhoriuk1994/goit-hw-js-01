d9c824ca88e82b4f9302b40b0b584667d3ea86bb
Artem Horiuk <62579471+artemhoriuk1994@users.noreply.github.com>
2022-05-28 17:00:26 +0300

    new

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,11 @@
 const colors = document.querySelector(".color");
 const changeBtn = document.querySelector(".change-color");
 
-const hex = function getRandomHexColor() {
+function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+}
 changeBtn.addEventListener("click", () => {
-  document.body.style.backgroundColor = hex();
-  colors.textContent = hex();
+  const colorStyle = getRandomHexColor();
+  document.body.style.background = colorStyle;
+  colors.textContent = colorStyle;
 });
